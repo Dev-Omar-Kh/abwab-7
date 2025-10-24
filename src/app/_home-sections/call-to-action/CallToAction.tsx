4eb20ea0fb7dab85db998795b8993cc71f4921d1
unknown <omar.er3434@gmail.com>
2025-10-24 20:22:23 +0300

    Add Call To Action Section 2

--- src/app/_home-sections/call-to-action/CallToAction.tsx
+++ src/app/_home-sections/call-to-action/CallToAction.tsx
@@ -51,7 +51,7 @@ export default function CallToAction() {
                 <div className='px-2.5 py-1.5 bg-blackColor/5 rounded-full flex items-center gap-2.5'>
                     <GoDotFill className='text-greenColor shrink-0' />
                     <p className='text-sm text-blackColor'>
-                        <NumbersText lang='ar' number={100} className='text-base' />% {t('callToAction.note')}
+                        <NumbersText lang='ar' number={100} className='!text-lg' />% {t('callToAction.note')}
                     </p>
                 </div>
 
